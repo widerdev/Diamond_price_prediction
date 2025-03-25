329e5dd24f9632b44aa103e69ecf3d8559eb8ea5
Dev Chaudhary <devc1829@gmail.com>
2025-03-25 22:45:26 +0530

    few updates

--- src/DimondPricePrediction/utils/utils.py
+++ src/DimondPricePrediction/utils/utils.py
@@ -44,7 +44,7 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
     except Exception as e:
         logging.info('Exception occured during model training')
         raise customexception(e,sys)
-    
+# loading the model which we have saved    
 def load_object(file_path):
     try:
         with open(file_path,'rb') as file_obj:
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import os
+"""import os
 
 path="notebooks/research.ipynb"
 
@@ -8,3 +8,11 @@ os.makedirs(dir)
 
 with open(file,'w') as f:
     pass
+"""
+
+from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData
+
+customdataobj = CustomData(1.52,62.2,58.0,7.27,7.33,4.55,"Premium","F","VS2")
+
+data = customdataobj.get_data_as_dataframe()
+print(data)
