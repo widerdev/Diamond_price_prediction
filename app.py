4ba28b853c33126bf2d0ff4bbd143b465f82793f
Dev Chaudhary <devc1829@gmail.com>
2025-03-25 23:30:00 +0530

    few new updates

--- app.py
+++ app.py
@@ -18,6 +18,31 @@ def predict_datapoint():
         return render_template("form.html")
     
     else:
-        pass
+        # fetching data from form
+        data=CustomData(
+            
+            carat=float(request.form.get('carat')),
+            depth = float(request.form.get('depth')),
+            table = float(request.form.get('table')),
+            x = float(request.form.get('x')),
+            y = float(request.form.get('y')),
+            z = float(request.form.get('z')),
+            cut = request.form.get('cut'),
+            color= request.form.get('color'),
+            clarity = request.form.get('clarity')
+        )
+        # this is my final data
+        final_data=data.get_data_as_dataframe()
+        
+        predict_pipeline=PredictPipeline() #object
+        
+        pred=predict_pipeline.predict(final_data) #our prediction
+        
+        result=round(pred[0],2) #kept prediction in result
+        
+        return render_template("result.html",final_result=result)
 
-app.run()
+
+#execution begin
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=80)
